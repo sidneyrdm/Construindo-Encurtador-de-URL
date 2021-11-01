ba408ef3f473bb3c1684174c427ec582e2b75c00
sidneyrdm <sidneybritomal@gmail.com>
2021-11-01 11:56:16 -0300

    first commit - criacao do projeto base

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const api = (0, express_1.default)();
+api.get('/test', (req, res) => {
+    res.json({ success: true });
+});
+api.listen(5000, () => {
+    console.log('Express NOT Listening...');
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+ const api = express();
+
+ api.get('/test', (req: Request, res: Response) => {
+     res.json({ success: true })
+ })
+
+ api.listen(5000, () => {
+     console.log('Express NOT Listening...')
+ })
